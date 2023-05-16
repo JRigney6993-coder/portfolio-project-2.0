f7357f6b6363e7a2d0bd6fcb9a7c03117656b7c6
ContriverJake <77342747+JRigney6993-coder@users.noreply.github.com>
2023-05-15 21:01:28 -0700

    near done

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,8 +7,8 @@ $(window).on("scroll", function () {
         if (scrollPosition >= 50) {
             header.css({
                 "background-color": "rgba(8, 6, 36, .3)",
-                "box-shadow": "0px 0px 30px -2px rgba(134, 85, 255, .3)",
-                "backdrop-filter": "blur(1.5px)"
+                "box-shadow": "0px 0px 50px 0px rgba(134, 85, 255, .3)",
+                "backdrop-filter": "blur(3px)"
             });
         }
     } else if (scrollPosition === 0) {
@@ -49,4 +49,45 @@ $('.card').each(function () {
     }
 });
 
+$(document).ready(function() {
+    // Define your project images
+    var projects = {
+        'student-management-panel': ['./assets/images/projects/student_management/Image_4.jpg', './assets/images/projects/student_management/Image_1.jpg', './assets/images/projects/student_management/Image_2.jpg', './assets/images/projects/student_management/Image_3.jpg'],
+        'image-geofinder': ['./assets/images/projects/image_geofinder/Image_1.jpg'],
+        'project3': ['image7.jpg', 'image8.jpg', 'image9.jpg']
+    };
+
+    // Listen for button clicks
+    $('.view-project').click(function() {
+        var project = $(this).data('project');
+        var images = projects[project];
+        
+        // Clear the carousel
+        $('.carousel-indicators').empty();
+        $('.carousel-inner').empty();
+
+        // Load the images into the carousel
+        for (var i = 0; i < images.length; i++) {
+            $('<li>').attr({
+                'data-bs-target': '#carouselExampleIndicators',
+                'data-bs-slide-to': i
+            }).appendTo('.carousel-indicators');
+        
+            $('<div>').attr('class', 'carousel-item')
+            .append($('<img>').attr({
+                'class': 'd-block w-100',
+                'src': images[i],
+                'alt': 'Slide ' + (i + 1)
+            })).appendTo('.carousel-inner');
+        }
+        
+        // Set the first carousel item to be the active one
+        $('.carousel-item').first().addClass('active');
+        $('.carousel-indicators > li').first().addClass('active');
+        
+        // Show the modal
+        $('#projectModal').modal('show');
+        });
+        });
+        
 
